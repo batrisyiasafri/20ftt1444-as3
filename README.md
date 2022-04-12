fe7510a7141e0227d1b2f609b53f41ebe30c7580
batrisyiasafri <20FTT1444@student.pb.edu.bn>
2022-04-12 21:04:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+
+<!-- DESIGN REFERENCES -->
+
+1. Creating rounded image
+Title: How To Make An Artsy Half Rounded Image in Tailwind CSS
+Author: Bilal Tahir
+https://btahir.medium.com/how-to-make-an-artsy-half-rounded-image-in-tailwind-css-70fe25a9de0 
+
+2. Creating a card
+Title: Create a Simple Responsive Card Grid With Tailwind CSS Examples
+Author: larainfo
+https://larainfo.com/blogs/create-a-simple-responsive-card-grid-with-tailwind-css-examples
+
+3. Tailwind CSS Customization
+Title:  tailwindcss
+Author: tailwindcss
+https://tailwindcss.com/docs/aspect-ratio
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue' 
+</script>
+
+<template>
+
+  <body class="bg-blue-100 w-screen h-screen">
+
+  <div class="flex items-center justify-center">
+  </div>
+
+  <div class="grid lg:grid-cols-4">
+    <div class="w-full p-4 lg:w-80">
+        <div class="p-8 bg-pink-200 rounded shadow-md">
+          <h2 class="text-2xl font-bold">Profile</h2>
+          <br>
+           <img class="mx-auto rounded-extraLarge, rounded-tr-extraLarge, rounded-tl-extraLarge, rounded-br-extraLarge, rounded-bl-extraLarge rounded-tr-extraLarge rounded-bl-extraLarge rounded-tl-extraLarge rounded-br-extraLarge object-contain h-60 w-100" alt="mypic" src="./assets/mypic.jpg"/>
+           <br>
+           <p class="text-grey-200">Name: Batrisyia Safri</p>
+           <p class="text-grey-200">Student ID: 20FTT1444</p>
+
+        </div>
+      </div>
+
+    <div class="w-full p-4 lg:w-80">
+        <div class="p-8 bg-green-200 rounded shadow-md">
+            <h2 class="text-2xl font-bold">Interests</h2>
+            <ul class="list-disc">
+            <li class="text-grey-200">Learning PHP, Tailwind and Vue</li>
+            <li class="text-grey-200">Cooking Malaysian Cuisine</li>
+            <li class="text-grey-200">Making Coffee</li>
+            <li class="text-grey-200">Reading a History Book</li>
+            </ul>
+        </div>
+
+        <br>
+
+          <div class="p-8 bg-amber-200 rounded shadow-md">
+            <h2 class="text-2xl font-bold">Hobbies</h2>
+            <ul class="list-disc">
+            <li class="text-grey-200">Go for a walk</li>
+            <li class="text-grey-200">Visit my pets</li>
+            <li class="text-grey-200">Play video games</li>
+            <li class="text-grey-200">Sleep</li>
+             </ul>
+          </div>
+    </div>
+</div>
+
+
+   
+  </body>
+  
+
+</template>
+
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
